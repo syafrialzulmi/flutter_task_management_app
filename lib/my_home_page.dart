c58f78fb24588b26e214118ff2534f5564447eb8
Syafrial Zulmi <syafrialzulmi@gmail.com>
2022-02-02 22:36:41 +0700

    bottomNavigationBar

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task_management_app/about.dart';
+import 'package:flutter_task_management_app/feed.dart';
 // import 'package:flutter_task_management_app/feed.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,11 +9,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    FeedPage(),
+    About(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: const About(),
+      body: _children.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.verified_user),
+            label: 'About Us',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
